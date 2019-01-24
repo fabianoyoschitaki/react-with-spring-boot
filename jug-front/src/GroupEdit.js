19bd8028c8b8d944d2b6eb47904caff792ee6585
Fabiano <fabianoyoschitaki@gmail.com>
2019-01-24 18:07:17 -0200

    groupedit component

---
+++ jug-front/src/GroupEdit.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+
+class GroupEdit extends Component {
+	emptyItem = {
+		name: '',
+		address: '',
+		city: '',
+		stateOrProvince: '',
+		country: '',
+		postalCode: ''
+	};
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			item: this.emptyItem
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+}
